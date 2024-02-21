bfd35cb2f6e32c29ac9d3a6f00b67fa647d6aee8
miracline2 <francesca4518@gmail.com>
2024-02-21 08:34:42 +0530

    Added a message request whith the button yes and no

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.scss']
+})
+export class MessageComponent implements OnInit {
+  question : any
+  gif :any
+  noBtn : any;
+  yesBtn:any;
+
+  message = 'Hey Vishnu!! Will You Marry Me?'
+
+ ngOnInit(): void {
+  this.question= document.querySelector(".question");
+ this. noBtn=document.querySelector(".noButton");
+ this. gif = document.querySelector(".gif");
+ this. yesBtn = document.querySelector(".yesButton");
+}
+answerYes(){
+  if(this.yesBtn){
+    this.message = 'Aww!! I Love You 😍🥰'
+  }
+  this.gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif"
+  
+}
+answerNo(){
+  const noBtnRect = this.noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  this.noBtn.style.left = randomX + "px";
+  this.noBtn.style.top = randomY + "px";
+}
+
+
+}
