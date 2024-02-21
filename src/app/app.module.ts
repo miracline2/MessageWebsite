87a8c7289cf7e2aeb71d0e8fb44c4f31dbd8905f
miracline2 <francesca4518@gmail.com>
2024-02-21 23:15:06 +0530

    created front page for website and adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageComponent } from './message/message.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessageComponent
+    MessageComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule,
